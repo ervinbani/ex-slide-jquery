c634b094bd43deaa080dd24a730ec8e8a959ecaa
ervinbani <bani.ervin@gmail.com>
2018-04-03 15:25:27 +0200

    es completto

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $("#hide").click(function(){
+        $("#p1").hide();
+        $("#hide").hide();
+        $("#show").show();
+
+    });
+    $("#show").click(function(){
+        $("#p1").show();
+        $("#hide").show();
+        $("#show").hide();
+    });
+    $("#hide2").click(function(){
+        $("#p2").hide();
+        $("#hide2").hide();
+        $("#show2").show();
+
+    });
+    $("#show2").click(function(){
+        $("#p2").show();
+        $("#hide2").show();
+        $("#show2").hide();
+    });
+
+
+
+
+
+});
